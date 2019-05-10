4a69cd5a4bb5d9060a72ba28db82a83bebb79c9b
Reyaad Rafeek <reyaad510@gmail.com>
2019-05-10 12:30:35 -0400

    Completed .get post by id functionality

--- tests/tests-router.js
+++ tests/tests-router.js
@@ -5,6 +5,9 @@ const Tests = require('../data/db');
 
 const router = express.Router();
 
+
+// Read - Retrieving posts on http://localhost:9000/api/posts
+
 router.get('/', async (req, res) => {
     try {
         const tests = await Tests.find();
@@ -15,6 +18,33 @@ router.get('/', async (req, res) => {
          })
     }
 })
+
+router.get('/:id', async (req, res) => {
+
+    const  { id } = req.params;
+
+    try {
+    const test = await Tests.findById(id);
+    if(test) {
+        res.status(200).json(test);
+    } else {
+        res.status(404).json({ 
+            message: "The post with the specified ID does not exist." 
+         })
+    }
+    } catch(err) {
+        res.status(500).json({
+            error: "The posts info could not be retrieved."
+         })
+    }
+
+})
+
+
+
+
+
+
 
 
 module.exports = router;
